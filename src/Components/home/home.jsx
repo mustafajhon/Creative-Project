acee882e1bc962524e62d9135eb7189095e9354a
mustafa <mustafanaiby@gmail.com>
2023-02-18 07:39:39 +0330

    h

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 class Home extends Component {
-  state = { myName: "Mustafa", Age: 21, Goal: "Travel Around The World" };
+  state = { myName: "Mustafa", Age: 22, Goal: "Travel Around The World" };
+
+  UpHandler = () => {
+    this.setState({ Age: this.state.Age + 1 });
+  };
+  DownHandler = () => {
+    this.setState({ Age: this.state.Age - 1 });
+  };
+
   render() {
     return (
       <>
@@ -8,6 +16,10 @@ class Home extends Component {
           <h2 className="text text-center">
             Hello {this.state.myName} your are {this.state.Age} years Old.
           </h2>
+          <div className="btns">
+            <button onClick={this.UpHandler}>A</button>
+            <button onClick={this.DownHandler}>V</button>
+          </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
             nisi ipsa temporibus commodi veniam dignissimos laborum consequuntur
